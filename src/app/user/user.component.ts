d84609a727153843b2bb76d58c3e241559d2ce89
Agustin Mulet <agustin.mulet@gmail.com>
2020-09-14 00:35:21 -0300

    Added Read Single User

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { HttpClient } from '@angular/common/http';
-
-const wait = (cb) => (setTimeout(cb, 2000));
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-user',
@@ -16,16 +15,35 @@ export class UserComponent implements OnInit {
   user: any;
   constructor(
     private route: ActivatedRoute,
-    private http: HttpClient
+    private apollo: Apollo
     ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
-      this.http.get<any>(`http://localhost:3000/users/${params.get('id')}`).subscribe(data => {
-        wait(() => {
-          this.user = data;
-          this.loading = false;
-        });
+      this.apollo
+      .watchQuery({
+        query: gql`
+          query getUser($id: String!){
+            user(id: $id) {
+              id
+              name
+              address
+              birthday
+              posts {
+                id
+                title
+              }
+            }
+          }
+        `,
+        variables: {
+          id: params.get('id')
+        }
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.user = result.data?.user;
+        this.loading = result.loading;
+        this.error = result.error;
       });
     });
   }
